ee85161ec9220ebd7ccda18505fe196cccc08892
gvanbeelen <gvanbeelen@hotmail.com>
2015-04-23 13:25:24 +0000

    ManyGreetings.py toegevoegd

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,6 +1,5 @@
 print('What is your name?')
 yourName=input()
 print('How many greetings?')
-number=int(input())
-for i in range(number):
-    print ('Hello, ' + yourName + '!')
+number=input()
+print(number + ' x Hello, ' + yourName + '!')
